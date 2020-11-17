d87cd5fce363b913b8c91115499d5a12b014f7dc
ninami1001 <ninami1001@gmail.com>
2020-11-17 23:35:22 +0100

    Add project files.

---
+++ WebApplication1/Controllers/JeloController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class JeloController : Controller
+    {
+        private TestBEntities db = new TestBEntities();
+
+        // GET: Jelo
+        public ActionResult Index()
+        {
+            var jeloes = db.Jeloes.Include(j => j.Restoran);
+            return View(jeloes.ToList());
+        }
+
+        public ActionResult Random()
+        {
+
+            var jeloDBs = db.Jeloes
+                  .OrderBy(c => Guid.NewGuid())
+                  .FirstOrDefault();
+
+            return View(jeloDBs);
+        }
+
+
+        // GET: Jelo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jelo jelo = db.Jeloes.Find(id);
+            if (jelo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jelo);
+        }
+
+        // GET: Jelo/Create
+        public ActionResult Create()
+        {
+            ViewBag.RestoranID = new SelectList(db.Restorans, "Id", "Naziv");
+            return View();
+        }
+
+        // POST: Jelo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Naziv,Cijena,RestoranID")] Jelo jelo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jeloes.Add(jelo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RestoranID = new SelectList(db.Restorans, "Id", "Naziv", jelo.RestoranID);
+            return View(jelo);
+        }
+
+        // GET: Jelo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jelo jelo = db.Jeloes.Find(id);
+            if (jelo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RestoranID = new SelectList(db.Restorans, "Id", "Naziv", jelo.RestoranID);
+            return View(jelo);
+        }
+
+        // POST: Jelo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Naziv,Cijena,RestoranID")] Jelo jelo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jelo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RestoranID = new SelectList(db.Restorans, "Id", "Naziv", jelo.RestoranID);
+            return View(jelo);
+        }
+
+        // GET: Jelo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jelo jelo = db.Jeloes.Find(id);
+            if (jelo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jelo);
+        }
+
+        // POST: Jelo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jelo jelo = db.Jeloes.Find(id);
+            db.Jeloes.Remove(jelo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/RestoranController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RestoranController : Controller
+    {
+        private TestBEntities db = new TestBEntities();
+
+        // GET: Restoran
+        public ActionResult Index()
+        {
+            return View(db.Restorans.ToList());
+        }
+
+        // GET: Restoran/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restoran restoran = db.Restorans.Find(id);
+            if (restoran == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restoran);
+        }
+
+        // GET: Restoran/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restoran/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Naziv,Telefon,Adresa")] Restoran restoran)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restorans.Add(restoran);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restoran);
+        }
+
+        // GET: Restoran/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restoran restoran = db.Restorans.Find(id);
+            if (restoran == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restoran);
+        }
+
+        // POST: Restoran/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Naziv,Telefon,Adresa")] Restoran restoran)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(restoran).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restoran);
+        }
+
+        // GET: Restoran/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restoran restoran = db.Restorans.Find(id);
+            if (restoran == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restoran);
+        }
+
+        // POST: Restoran/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Restoran restoran = db.Restorans.Find(id);
+            db.Restorans.Remove(restoran);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
